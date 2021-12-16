09960338858e618df2e969855765a09020d1c5f8
kkd5429 <95455968+kkd5429@users.noreply.github.com>
2021-12-15 23:06:05 -0500

    Record time get data from workday table

--- routes/record.js
+++ routes/record.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+var config = require("../dbconfig");
+const sql = require("mysql");
 
 router.get('/', (req, res) => {
-  res.render('record');
+  try {
+    var con = sql.createConnection(config);
+    con.connect();
+    con.query("SELECT * FROM workday", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      res.json(result);
+    });
+    con.end();
+  } catch (error) {
+    console.log(error);
+  }
+  // res.render('record');
 });
 
 module.exports = router;
