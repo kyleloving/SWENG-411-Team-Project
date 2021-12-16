0917f71ecf64b6a561735607aa75ff530cd7954e
kkd5429 <95455968+kkd5429@users.noreply.github.com>
2021-12-15 23:10:07 -0500

    Moved record time to Timecard

--- routes/record.js
+++ routes/record.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const router = express.Router();
-var config = require("../dbconfig");
-const sql = require("mysql");
+
 
 router.get('/', (req, res) => {
-  try {
-    var con = sql.createConnection(config);
-    con.connect();
-    con.query("SELECT * FROM workday", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-      res.json(result);
-    });
-    con.end();
-  } catch (error) {
-    console.log(error);
-  }
-  // res.render('record');
+  res.render('record');
 });
 
 module.exports = router;
--- routes/timecard.js
+++ routes/timecard.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+var config = require("../dbconfig");
+const sql = require("mysql");
 
 router.get('/', (req, res) => {
+  try {
+    var con = sql.createConnection(config);
+    con.connect();
+    con.query("SELECT * FROM workday", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      // res.json(result);
+    });
+    con.end();
+  } catch (error) {
+    console.log(error);
+  }
   res.render('timecard');
 });
 
